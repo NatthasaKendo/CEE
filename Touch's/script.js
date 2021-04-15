9f8428367791ef1048d86fa49c673cd261953f05
Tuchtham Sungkameka <touch0869@gmail.com>
2021-04-15 23:04:04 +0700

    Committed Touch's files

---
+++ Touch's/script.js
@@ -0,0 +1,35 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyCmIN_eGfPuTFaJk336JWA3WLDYtqrH1WI",
+    authDomain: "tspk-cee-2021.firebaseapp.com",
+    projectId: "tspk-cee-2021",
+    storageBucket: "tspk-cee-2021.appspot.com",
+    messagingSenderId: "291245415103",
+    appId: "1:291245415103:web:c00d44912ad9c86e9f840a",
+    measurementId: "G-RGMWM4R3HK"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+const db = firebase.firestore();
+const test_name_ref = db.collection("test_name");
+
+function addName() {
+    console.log("add name");
+    const name = document.getElementById('fname').value;
+    console.log(name);
+    db.collection("test_name").add({
+        name: name,
+    })
+        .then((docRef) => {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch((error) => {
+            console.error("Error adding document: ", error);
+        });
+
+}
+
+addName("test");
